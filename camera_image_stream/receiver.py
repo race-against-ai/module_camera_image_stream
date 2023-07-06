fccaac380ddd1e3e2bdfad78d84a4fbfcc1aca5e
Arne Hilbig <666arnehilbig@gmail.com>
2023-07-06 14:19:43 +0200

    Refactoring Code; Adding Docstrings

--- camera_image_stream/receiver.py
+++ camera_image_stream/receiver.py
@@ -8,30 +8,37 @@ import cv2
 RASPBERRY_IP = "192.168.1.100"
 RASPBERRY_PORT = 8000
 
-VIDEO_CAPTURE = cv2.VideoCapture(f"udp://{RASPBERRY_IP}:{RASPBERRY_PORT}", cv2.CAP_FFMPEG)
-if not VIDEO_CAPTURE.read()[0]:
-    VIDEO_CAPTURE = cv2.VideoCapture(f"tcp://{RASPBERRY_IP}:{RASPBERRY_PORT}", cv2.CAP_FFMPEG)
-
-SHOW_PREVIEW = False
-
 PUB_ADDRESS = "ipc:///tmp/RAAI/camera_frame.ipc"
-PUB_SENDER = Pub0()
-PUB_SENDER.listen(PUB_ADDRESS)
+
+video_capture = cv2.VideoCapture(f"udp://{RASPBERRY_IP}:{RASPBERRY_PORT}", cv2.CAP_FFMPEG)
+if not video_capture.read()[0]:
+    video_capture = cv2.VideoCapture(f"tcp://{RASPBERRY_IP}:{RASPBERRY_PORT}", cv2.CAP_FFMPEG)
+
+pub_sender = Pub0()
+pub_sender.listen(PUB_ADDRESS)
 
 
 # Main Loop
-def main():
+def main(show_preview: bool = False):
+    """Receives the frame from the raspberry pi.
+
+    Args:
+        show_preview (bool): If True it will show the received frame from the raspberry pi.
+
+    Raises:
+        ConnectionError: If the connection has been interrupted and the next frame could not be read.
+    """
     while True:
-        success, frame = VIDEO_CAPTURE.read()
+        success, frame = video_capture.read()
 
         if not success:
             raise ConnectionError("Could not read the next frame. Check the camera.")
 
         frame_np_array = np.array(frame)
         frame_bytes = frame_np_array.tobytes()
-        PUB_SENDER.send(frame_bytes)
+        pub_sender.send(frame_bytes)
 
-        if SHOW_PREVIEW:
+        if show_preview:
             cv2.imshow("frame", frame)
             cv2.waitKey(1)
 
