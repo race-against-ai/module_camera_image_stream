7707dac9ccf092fe5b6819ae81db75f91886a4a9
Arne Hilbig <666arnehilbig@gmail.com>
2024-01-10 09:33:16 +0100

    Made it use jsonschema and made it buildable

--- camera_image_stream/receiver.py
+++ camera_image_stream/receiver.py
@@ -1,20 +1,46 @@
-# Imports
+"""Receives a video stream from a Raspberry Pi and publishes it to a pynng publisher."""
+# Copyright (C) 2023, NG:ITL
+
 from pathlib import Path
+from json import load, dump
+
 from pynng import Pub0
-from json import load
+from jsonschema import validate
 import numpy as np
 import cv2
 
 
+FILE_DIR = Path(__file__).parent
+CONFIG_FILE_NAME = "camera_image_stream_config.json"
+
+
+def create_config_file() -> None:
+    """Creates a config file with default values."""
+    with open(FILE_DIR / "templates/config.json", "r", encoding="utf-8") as template:
+        with open(CONFIG_FILE_NAME, "x", encoding="utf-8") as config_file:
+            dump(load(template), config_file, indent=4)
+
+
 def main() -> None:
-    config_path = Path().cwd() / "camera_image_stream_config.json"
-    with open(config_path, "r") as config_file:
+    """Receives a video stream from a Raspberry Pi and publishes it to a pynng publisher.
+
+    Raises:
+        ConnectionError: If the next frame could not be read from the video stream.
+    """
+    if not Path(CONFIG_FILE_NAME).is_file():
+        create_config_file()
+
+    with open(CONFIG_FILE_NAME, "r", encoding="utf-8") as config_file:
         config = load(config_file)
+
+        with open(FILE_DIR / "schema/config_schema.json", "r", encoding="utf-8") as schema_file:
+            validate(config, load(schema_file))
+
         camera_stream = config["pynng"]["publishers"]["camera_image_publisher"]
         raspberry_pi_config = config["raspberry_pi"]
         show_preview = config["show_preview"]
 
-    video_capture = cv2.VideoCapture(f"udp://{raspberry_pi_config['ip']}:{raspberry_pi_config['port']}", cv2.CAP_FFMPEG)
+    video_capture = cv2.VideoCapture(f"udp://{raspberry_pi_config['ip']}:{raspberry_pi_config['port']}", cv2.CAP_FFMPEG)  # type: ignore[call-arg]
     pub_sender = Pub0(listen=camera_stream["address"])
 
     while True:
